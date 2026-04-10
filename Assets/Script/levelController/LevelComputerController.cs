806b65f02d505ca16159c4a55d65ee9eb8eb5a36
liuxy039 <159973978+liuxy039@users.noreply.github.com>
2026-04-10 17:21:44 +0800

    动画修复2

--- Assets/Script/levelController/LevelComputerController.cs
+++ Assets/Script/levelController/LevelComputerController.cs
@@ -1,13 +1,26 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
+using System.Linq;
+using UnityEngine.SceneManagement;
+using System;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
 
 public class LevelComputerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    LevelFileManager  levelFileManager;
     void Start()
     {
-        
+        levelFileManager = GetComponent<LevelFileManager>();
+
+    }
+
+
+    public void Deleteblock()
+    {
+        File.Delete(Path.Combine(levelFileManager.GetFolderPath(), "block.p.txt"));     
     }
 
     // Update is called once per frame
